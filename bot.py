f72c2670fb770cb4e8c3a326f3a53912a734a391
Vasin-AD <anton_v2007@mail.ru>
2024-03-06 18:03:23 +0300

    Fix 10FIZ

--- bot.py
+++ bot.py
@@ -12,6 +12,16 @@ from telebot import TeleBot
 
 state_storage = StateMemoryStorage()
 
+day_to_number = {
+    "Понедельник" : 0, 
+    "Вторник" : 1, 
+    "Среда" : 2,
+    "Четверг" : 3,
+    "Пятница" : 4,
+    "Суббота" : 5, 
+    "Воскресенье" : 6
+}
+
 
 token = os.environ['TG_TOKEN']
 bot = TeleBot(token, state_storage=state_storage)
@@ -59,7 +69,6 @@ def schedule(message):
     bot.send_message(message.chat.id, "Выбери свой класс:", reply_markup=markup)
     
 
-
 @bot.message_handler(state=ScheduleStates.waiting_class)
 def get_class(message):
     number_class = int(message.text.split()[0])
@@ -75,12 +84,13 @@ def get_class(message):
     bot.send_message(message.chat.id, "Выбери букву класса: ", reply_markup=markup)
     bot.set_state(message.from_user.id, ScheduleStates.waiting_mark, message.chat.id)
 
+
 @bot.message_handler(state=ScheduleStates.waiting_mark)
-def get_day(message):
+def get_mark(message):
     mark = message.text
 
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-         data['mark'] = mark
+        data['mark'] = mark
     
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for i in ['Понедельник','Вторник','Среда', 'Четверг','Пятница']:
@@ -88,8 +98,32 @@ def get_day(message):
         markup.add(btn)
     
     bot.send_message(message.chat.id, "Выбери день", reply_markup=markup)
-    bot.set_state(message.from_user.id, ScheduleStates.waiting_mark, message.chat.id)
+    bot.set_state(message.from_user.id, ScheduleStates.waiting_day, message.chat.id)
 
 
+@bot.message_handler(state=ScheduleStates.waiting_day)
+def get_day(message):
+    day = message.text
+
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        
+        _mark = data['mark']
+        _class = data['number_class']
+
+    try:
+        excel_data = pd.read_excel(f'./data/{_class} Класс.xlsx', sheet_name=f"{_mark}", header=None)
+        ans = [lesson for lesson in excel_data[day_to_number[day]]]
+        ans = '\n'.join(ans)
+        bot.send_message(message.chat.id, ans, reply_markup=types.ReplyKeyboardRemove())
+        bot.delete_state(message.from_user.id, message.chat.id)
+    except ValueError:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for i in ['А', 'Б', 'В', 'Г', 'Т', 'Т(ФИЗ)', 'Т(ИНФ)', 'Е', 'Э', 'Г', 'Г(ИСТ)', 'Г(АНГ)']:
+            btn = types.KeyboardButton(i)
+            markup.add(btn)
+        bot.send_message(message.chat.id, "Такой буквы нет, выбери другую букву: ", reply_markup=markup)
+        bot.set_state(message.from_user.id, ScheduleStates.waiting_mark, message.chat.id)
+        
+            
 
 bot.polling()
